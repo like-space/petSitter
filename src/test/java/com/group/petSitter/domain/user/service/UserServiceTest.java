b2336a0b3049d2e7f68adcb10bd5bdea2e062404
sohyounsoo <sohyounsoo@gmail.com>
2024-04-08 20:29:25 +0900

    feat: add user

---
+++ src/test/java/com/group/petSitter/domain/user/service/UserServiceTest.java
@@ -0,0 +1,109 @@
+package com.group.petSitter.domain.user.service;
+
+import com.prgrms.nabmart.domain.user.User;
+import com.prgrms.nabmart.domain.user.exception.NotFoundUserException;
+import com.prgrms.nabmart.domain.user.repository.UserRepository;
+import com.prgrms.nabmart.domain.user.service.request.FindUserCommand;
+import com.prgrms.nabmart.domain.user.service.request.RegisterUserCommand;
+import com.prgrms.nabmart.domain.user.service.response.FindUserDetailResponse;
+import com.prgrms.nabmart.domain.user.support.UserFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Nested
+    @DisplayName("getOrRegisterUser 메서드 실행 시")
+    class GetOrRegisterUserTest {
+
+        RegisterUserCommand registerUserCommand = UserFixture.registerUserCommand();
+
+        @Test
+        @DisplayName("성공: User가 존재하면 User 반환")
+        void getUserWhenUserExists() {
+            //given
+            User user = UserFixture.user();
+            given(userRepository.findByProviderAndProviderId(any(), any())).willReturn(
+                Optional.ofNullable(user));
+
+            //when
+            userService.getOrRegisterUser(registerUserCommand);
+
+            //then
+            then(userRepository).should(times(0)).save(any());
+        }
+
+        @Test
+        @DisplayName("성공: User가 존재하지 않으면 User 생성 후 저장")
+        void getUserWhenUserNotFound() {
+            //given
+            given(userRepository.findByProviderAndProviderId(any(), any())).willReturn(
+                Optional.empty());
+
+            //when
+            userService.getOrRegisterUser(registerUserCommand);
+
+            //then
+            then(userRepository).should(times(1)).save(any());
+        }
+    }
+
+    @Nested
+    @DisplayName("findUser 메서드 실행 시")
+    class FindUserTest {
+
+        FindUserCommand findUserCommand = UserFixture.findUserCommand();
+
+        @Test
+        @DisplayName("성공")
+        void success() {
+            //given
+            User user = UserFixture.user();
+
+            given(userRepository.findById(any())).willReturn(Optional.ofNullable(user));
+
+            //when
+            FindUserDetailResponse result = userService.findUser(findUserCommand);
+
+            //then
+            assertThat(result.nickname()).isEqualTo(user.getNickname());
+            assertThat(result.email()).isEqualTo(user.getEmail());
+            assertThat(result.provider()).isEqualTo(user.getProvider());
+            assertThat(result.providerId()).isEqualTo(user.getProviderId());
+            assertThat(result.userRole()).isEqualTo(user.getUserRole());
+            assertThat(result.userGrade()).isEqualTo(user.getUserGrade());
+        }
+
+        @Test
+        @DisplayName("예외: 존재하지 않는 User")
+        void throwExceptionWhenUserNotFound() {
+            //given
+            given(userRepository.findById(any())).willReturn(Optional.empty());
+
+            //when
+            //then
+            assertThatThrownBy(() -> userService.findUser(findUserCommand))
+                .isInstanceOf(NotFoundUserException.class);
+        }
+    }
+}
---
+++ src/test/java/com/group/petSitter/domain/user/support/UserFixture.java
@@ -0,0 +1,88 @@
+package com.group.petSitter.domain.user.support;
+
+import com.prgrms.nabmart.domain.user.User;
+import com.prgrms.nabmart.domain.user.UserGrade;
+import com.prgrms.nabmart.domain.user.UserRole;
+import com.prgrms.nabmart.domain.user.service.request.FindUserCommand;
+import com.prgrms.nabmart.domain.user.service.request.RegisterUserCommand;
+import com.prgrms.nabmart.domain.user.service.response.FindUserDetailResponse;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class UserFixture {
+
+    private static final Long USER_ID = 1L;
+    private static final String NICKNAME = "닉네임";
+    private static final String EMAIL = "email@example.com";
+    private static final String PROVIDER = "provider";
+    private static final String PROVIDER_ID = "providerId";
+
+    private static final String ADDRESS = "기본 배송지";
+    private static final UserRole USER_ROLE = UserRole.ROLE_USER;
+    private static final UserGrade USER_GRADE = UserGrade.NORMAL;
+    private static final UserRole EMPLOYEE = UserRole.ROLE_EMPLOYEE;
+
+    public static User user() {
+        return User.builder()
+            .nickname(NICKNAME)
+            .email(EMAIL)
+            .provider(PROVIDER)
+            .providerId(PROVIDER_ID)
+            .userRole(USER_ROLE)
+            .userGrade(USER_GRADE)
+            .address(ADDRESS)
+            .build();
+    }
+
+    public static User userWithUserId() {
+        User user = User.builder()
+            .nickname(NICKNAME)
+            .email(EMAIL)
+            .provider(PROVIDER)
+            .providerId(PROVIDER_ID)
+            .userRole(USER_ROLE)
+            .userGrade(USER_GRADE)
+            .address(ADDRESS)
+            .build();
+
+        ReflectionTestUtils.setField(user, "userId", USER_ID);
+        return user;
+    }
+
+    public static User employee() {
+        return User.builder()
+            .nickname(NICKNAME)
+            .email(EMAIL)
+            .provider(PROVIDER)
+            .providerId(PROVIDER_ID)
+            .userRole(EMPLOYEE)
+            .userGrade(USER_GRADE)
+            .address(ADDRESS)
+            .build();
+    }
+
+    public static RegisterUserCommand registerUserCommand() {
+        return RegisterUserCommand.builder()
+            .nickname(NICKNAME)
+            .email(EMAIL)
+            .provider(PROVIDER)
+            .providerId(PROVIDER_ID)
+            .userRole(USER_ROLE)
+            .userGrade(USER_GRADE)
+            .build();
+    }
+
+    public static FindUserDetailResponse findUserDetailResponse() {
+        return new FindUserDetailResponse(
+            USER_ID,
+            NICKNAME,
+            EMAIL,
+            PROVIDER,
+            PROVIDER_ID,
+            USER_ROLE,
+            USER_GRADE);
+    }
+
+    public static FindUserCommand findUserCommand() {
+        return FindUserCommand.from(USER_ID);
+    }
+}
